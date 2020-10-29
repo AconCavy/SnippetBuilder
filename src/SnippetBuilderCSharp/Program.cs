efa5048760da0f484a6f9ab623b1c39071c49e2b
AconCavy <aconcavy@gmail.com>
2020-10-30 02:20:14 +0900

    Modify Program to escape invalid paths

--- src/SnippetBuilderCSharp/Program.cs
+++ src/SnippetBuilderCSharp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SnippetBuilderCSharp
@@ -21,6 +23,13 @@ namespace SnippetBuilderCSharp
                 targets.AddRange(target.Split(" "));
             }
 
+            targets = targets.Where(x => File.Exists(x) || Directory.Exists(x)).ToList();
+            if (!targets.Any())
+            {
+                Console.WriteLine("No valid file or directory paths");
+                Close();
+            }
+
             Console.WriteLine($"Enter the output directory (default is {DefaultOutputDirectory})");
             var outputDirectory = Console.ReadLine();
             if (string.IsNullOrEmpty(outputDirectory)) outputDirectory = DefaultOutputDirectory;
@@ -35,6 +44,14 @@ namespace SnippetBuilderCSharp
             await new VisualStudioCodeSnippetsBuilder(targets, outputDirectory, outputName).BuildAsync();
 
             Console.WriteLine($"Complete! Look {outputDirectory}");
+
+            Close();
+        }
+
+        private static void Close()
+        {
+            Console.WriteLine("Enter any key to close");
+            _ = Console.ReadLine();
         }
     }
 }

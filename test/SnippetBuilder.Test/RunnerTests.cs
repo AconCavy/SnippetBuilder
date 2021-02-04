1018da1bbaf12c0b862dfb9e88af99a34ad850d8
AconCavy <aconcavy@gmail.com>
2021-02-05 03:51:16 +0900

    Add tests for Runner

---
+++ test/SnippetBuilder.Test/RunnerTests.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Moq;
+using NUnit.Framework;
+using SnippetBuilder.IO;
+using SnippetBuilder.Models;
+using SnippetBuilder.Snippets;
+
+namespace SnippetBuilder.Test
+{
+    public class RunnerTests
+    {
+        [Test]
+        public void RunAsyncWithInputTest()
+        {
+            var mockSnippet = new Mock<ISnippet>();
+            var mockRecipeSerializer = new Mock<IRecipeSerializer>();
+
+            var sut = new Runner(new[] {mockSnippet.Object}, mockRecipeSerializer.Object);
+
+            const string line = "--input ./sample.cs";
+            var args = line.Split(" ");
+
+            Assert.DoesNotThrowAsync(() => sut.RunAsync(args));
+            mockSnippet.Verify(x => x.BuildAsync(It.IsAny<Recipe>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Test]
+        public void RunAsyncWithRecipeTest()
+        {
+            var mockSnippet = new Mock<ISnippet>();
+            var mockRecipeSerializer = new Mock<IRecipeSerializer>();
+            mockRecipeSerializer.Setup(x => x.DeserializeAsync(It.IsAny<string[]>()))
+                .Returns(ToAsyncEnumerable(new[] {new Recipe {Paths = new[] {"sample.cs"}}}));
+
+            var sut = new Runner(new[] {mockSnippet.Object}, mockRecipeSerializer.Object);
+
+            const string line = "--recipes sample.json";
+            var args = line.Split(" ");
+
+            Assert.DoesNotThrowAsync(() => sut.RunAsync(args));
+            mockSnippet.Verify(x => x.BuildAsync(It.IsAny<Recipe>(), It.IsAny<CancellationToken>()), Times.Once);
+            mockRecipeSerializer.Verify(x => x.DeserializeAsync(It.IsAny<IEnumerable<string>>()), Times.Once);
+        }
+
+        [Test]
+        public void RunAsyncWithDuplicatedNameTest()
+        {
+            var recipes = new[] {new Recipe {Paths = new[] {"sample1.cs"}}, new Recipe {Paths = new[] {"sample2.cs"}}};
+            var mockSnippet = new Mock<ISnippet>();
+            var mockRecipeSerializer = new Mock<IRecipeSerializer>();
+            mockRecipeSerializer.Setup(x => x.DeserializeAsync(It.IsAny<string[]>()))
+                .Returns(ToAsyncEnumerable(recipes));
+
+            var sut = new Runner(new[] {mockSnippet.Object}, mockRecipeSerializer.Object);
+
+            const string line = "--recipes sample1.json sample2.json";
+            var args = line.Split(" ");
+
+            Assert.DoesNotThrowAsync(() => sut.RunAsync(args));
+            mockSnippet.Verify(x => x.BuildAsync(It.IsAny<Recipe>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
+            mockRecipeSerializer.Verify(x => x.DeserializeAsync(It.IsAny<IEnumerable<string>>()), Times.Once);
+        }
+
+        [Test]
+        public void RunAsyncWithInputAndRecipeTest()
+        {
+            var mockSnippet = new Mock<ISnippet>();
+            var mockRecipeSerializer = new Mock<IRecipeSerializer>();
+            mockRecipeSerializer.Setup(x => x.DeserializeAsync(It.IsAny<string[]>()))
+                .Returns(ToAsyncEnumerable(new[] {new Recipe {Paths = new[] {"sample.cs"}}}));
+
+            var sut = new Runner(new[] {mockSnippet.Object}, mockRecipeSerializer.Object);
+
+            const string line = "--input ./sample.cs --recipes sample.json";
+            var args = line.Split(" ");
+
+            Assert.DoesNotThrowAsync(() => sut.RunAsync(args));
+            mockSnippet.Verify(x => x.BuildAsync(It.IsAny<Recipe>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
+            mockRecipeSerializer.Verify(x => x.DeserializeAsync(It.IsAny<IEnumerable<string>>()), Times.Once);
+        }
+
+        [Test]
+        public void RunAsyncInteractTest()
+        {
+            var mockSnippet = new Mock<ISnippet>();
+            var mockRecipeSerializer = new Mock<IRecipeSerializer>();
+
+            var sut = new Runner(new[] {mockSnippet.Object}, mockRecipeSerializer.Object);
+
+            var args = Array.Empty<string>();
+            const string input = @"./sample1.cs ./sample2.cs
+./sample3.cs
+
+
+
+
+";
+            using var inputReader = new StringReader(input);
+            Console.SetIn(inputReader);
+
+            Assert.DoesNotThrowAsync(() => sut.RunAsync(args));
+            mockSnippet.Verify(x => x.BuildAsync(It.IsAny<Recipe>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Test]
+        public void RunAsyncWithMultipleSnippetTest()
+        {
+            var recipes = new[] {new Recipe {Paths = new[] {"sample1.cs"}}, new Recipe {Paths = new[] {"sample2.cs"}}};
+            var mockSnippet1 = new Mock<ISnippet>();
+            var mockSnippet2 = new Mock<ISnippet>();
+            var mockRecipeSerializer = new Mock<IRecipeSerializer>();
+            mockRecipeSerializer.Setup(x => x.DeserializeAsync(It.IsAny<string[]>()))
+                .Returns(ToAsyncEnumerable(recipes));
+
+            var sut = new Runner(new[] {mockSnippet1.Object, mockSnippet2.Object}, mockRecipeSerializer.Object);
+
+            const string line = "--recipes sample1.json sample2.json";
+            var args = line.Split(" ");
+
+            Assert.DoesNotThrowAsync(() => sut.RunAsync(args));
+            mockSnippet1.Verify(x => x.BuildAsync(It.IsAny<Recipe>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
+            mockSnippet2.Verify(x => x.BuildAsync(It.IsAny<Recipe>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
+        }
+
+        [Test]
+        public void RunAsyncWithInvalidArgsTest()
+        {
+            var mockSnippet = new Mock<ISnippet>();
+            var mockRecipeSerializer = new Mock<IRecipeSerializer>();
+            var sut = new Runner(new[] {mockSnippet.Object}, mockRecipeSerializer.Object);
+
+            const string line = "--foo bar";
+            var args = line.Split(" ");
+
+            Assert.DoesNotThrowAsync(() => sut.RunAsync(args));
+            mockSnippet.Verify(x => x.BuildAsync(It.IsAny<Recipe>(), It.IsAny<CancellationToken>()), Times.Never);
+        }
+
+        private static async IAsyncEnumerable<T> ToAsyncEnumerable<T>(IEnumerable<T> source)
+        {
+            foreach (var item in source) yield return item;
+        }
+    }
+}

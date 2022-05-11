11493a64f425d7e28c2a7391f7a5628c3444cbee
AconCavy <aconcavy@gmail.com>
2022-05-11 11:25:35 +0900

    Modify RecipeExtensions to check whitespaces

--- src/SnippetBuilder/Extensions/RecipeExtensions.cs
+++ src/SnippetBuilder/Extensions/RecipeExtensions.cs
@@ -6,8 +6,8 @@ public static class RecipeExtensions
 {
     public static bool Validate(this Recipe recipe)
     {
-        if (string.IsNullOrEmpty(recipe.Name)) return false;
-        if (string.IsNullOrEmpty(recipe.Output)) return false;
-        return recipe.Input is { } && !recipe.Input.Any(string.IsNullOrEmpty);
+        if (string.IsNullOrEmpty(recipe.Name) || string.IsNullOrWhiteSpace(recipe.Name)) return false;
+        if (string.IsNullOrEmpty(recipe.Output) || string.IsNullOrWhiteSpace(recipe.Output)) return false;
+        return recipe.Input is { } && !recipe.Input.Any(x => string.IsNullOrEmpty(x) || string.IsNullOrWhiteSpace(x));
     }
 }
